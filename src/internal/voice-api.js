768913b493640465e310d89ec2249cc24f121172
Chris Munn <Chris.Munn@genesys.com>
2017-08-21 11:10:14 -0300

    Adding call recording, send-dtmf, send-user-event, userdata functions, clear, redirect, delete-from-conference, set-forward/cancel-forward, voice login/logout and merge

--- src/internal/voice-api.js
+++ src/internal/voice-api.js
@@ -99,6 +99,47 @@ class VoiceApi {
     return response;
   }
 
+  async dndOn() {
+    this._log('Sending dnd-on...');
+    let response = await this._api.setDNDOn();
+    return response;
+  }
+
+  async dndOff() {
+    this._log('Sending dnd-off...');
+    let response = await this._api.setDNDOff();
+    return response;
+  }
+
+  async login() {
+    this._log('Sending voice login...');
+    let response = await this._api.loginVoice();
+    return response;
+  }
+
+  async logout() {
+    this._log('Sending voice logout...');
+    let response = await this._api.logoutVoice();
+    return response;
+  }
+
+  async setForward(forwardTo) {
+    this._log(`Sending set-forward with forwardTo [${forwardTo}]...`);
+    let response = await this._api.forward({
+      data: {
+        forwardTo: forwardTo
+      }
+    });
+
+    return response;
+  }
+
+  async cancelForward() {
+    this._log('Sending cancel-forward...');
+    let response = await this._api.cancelForward();
+    return response;
+  }
+
   async makeCall(destination) {
     this._log(`Sending make-call to destination [${destination}]...`);
     let response = await this._api.makeCall(
@@ -135,6 +176,23 @@ class VoiceApi {
     return response;
   }
 
+  async clearCall(connId) {
+    this._log(`Sending clear for call [${connId}]...`);
+    let response = await this._api.clear(connId);
+    return response;
+  }
+
+  async redirectCall(connId, destination) {
+    this._log(`Sending redirect for call [${connId}] with destination [${destination}]...`);
+    let response = await this._api.redirect(connId, {
+      data: {
+        destination: destination
+      }
+    });
+
+    return destination;
+  }
+
   async initiateConference(connId, destination) {
     this._log(`Sending initiate-conference to destination [${destination}] for call [${connId}]...`);
     let response = await this._api.initiateConference(connId, {
@@ -151,6 +209,17 @@ class VoiceApi {
     let response = await this._api.completeConference(connId, {
       data: {
         parentConnId: parentConnId
+      }
+    });
+
+    return response;
+  }
+
+  async deleteFromConference(connId, dnToDrop) {
+    this._log(`Sending delete-from-conference for call [${connId}] with dnToDrop [${dnToDrop}]...`);
+    let response = await this._api.deleteFromConference(connId, {
+      data: {
+        dnToDrop: dnToDrop
       }
     });
 
@@ -190,6 +259,17 @@ class VoiceApi {
     return response;
   }
 
+  async mergeCalls(connId, otherConnId) {
+    this._log(`Sending merge for call [${connId}] and otherConnId [${otherConnId}]...`);
+    let response = await this._api.merge(connId, {
+      data: {
+        otherConnId: otherConnId
+      }
+    });
+
+    return response;
+  }
+
   async reconnectCall(connId, heldConnId) {
     this._log(`Sending reconnect for call [${connId}] and heldConnId [${heldConnId}]...`);
     let response = await this._api.reconnect(connId, {
@@ -223,15 +303,81 @@ class VoiceApi {
     return response;
   }
 
-  async dndOn() {
-    this._log('Sending dnd-on...');
-    let response = await this._api.setDNDOn();
+  async attachUserData(connId, userData) {
+    this._log(`Sending attach-user-data for call [${connId}: ${JSON.stringify(data)}...`);
+    let response = await this._api.attachUserData(connId, {
+      data: {
+        userData: userData
+      }
+    });
     return response;
   }
 
-  async dndOff() {
-    this._log('Sending dnd-off...');
-    let response = await this._api.setDNDOff();
+  async updateUserData(connId, userData) {
+    this._log(`Sending update-user-data for call [${connId}: ${JSON.stringify(data)}...`);
+    let response = await this._api.updateUserData(connId, {
+      data: {
+        userData: userData
+      }
+    });
+    return response;
+  }
+
+  async deleteUserDataPair(connId, key) {
+    this._log(`Sending delete-user-data-pair for call [${connId}] with key [${key}]...`);
+    let response = await this._api.deleteUserDataPair(connId, {
+      data: {
+        key: key
+      }
+    });
+
+    return response;
+  }
+
+  async sendDtmf(connId, digits) {
+    this._log(`Sending send-dtmf for call [${connId}] with dtmfDigits [${digits}]...`);
+    let response = await this._api.sendDtmf(connId, {
+      data: {
+        dtmfDigits: digits
+      }
+    });
+
+    return response;
+  }
+
+  async sendUserEvent(data, callUuid) {
+    this._log(`Sending send-user-event with data [${JSON.stringify(data)}] and callUuid [${callUuid}]...`);
+    let response = await this._api.sendUserEvent({
+      data: {
+        userData: data,
+        callUuid: callUuid
+      }
+    });
+
+    return response;
+  }
+
+  async startRecording(connId) {
+    this._log(`Sending start-recording for call [${connId}]...`);
+    let response = await this._api.startRecording(connId);
+    return response;
+  }
+
+  async pauseRecording(connId) {
+    this._log(`Sending pause-recording for call [${connId}]...`);
+    let response = await this._api.pauseRecording(connId);
+    return response;
+  }
+
+  async resumeRecording(connId) {
+    this._log(`Sending resume-recording for call [${connId}]...`);
+    let response = await this._api.resumeRecording(connId);
+    return response;
+  }
+
+  async stopRecording(connId) {
+    this._log(`Sending stop-recording for call [${connId}]...`);
+    let response = await this._api.stopRecording(connId);
     return response;
   }
 }
