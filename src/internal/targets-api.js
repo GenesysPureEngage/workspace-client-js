aba43c8d10141fd652e980224fa3f72f91cc14ff
witchkun <nyatta.hen@gmail.com>
2017-10-18 16:54:09 +0300

    add remaining targets api functions (recents,favorites)

--- src/internal/targets-api.js
+++ src/internal/targets-api.js
@@ -12,6 +12,11 @@ class TargetsApi {
     }
   }
 
+  /**
+   * 
+   * @param {string} searchTerm
+   * @param {number} limit
+   */
   async search(searchTerm, limit) {
     this._log(`Searching targets with searchTerm [${searchTerm}]...`);
     const response = await this._api.get(searchTerm, {
@@ -19,6 +24,79 @@ class TargetsApi {
     });
 
     return response.data.targets;
+  }
+  
+  /**
+   * @param {TargetInformation} target
+   * @param {string} media
+   */
+  async addRecentTarget(target, media) {
+      const resp = await this._api.addRecentTargetWithHttpInfo({
+          data: {
+              target: target,
+              recentInformation: {
+                  media: media
+              }
+          }          
+      });
+      
+      return resp.response.body;
+  }
+  
+  /**
+   * @param {number} limit
+   */
+  async getRecentTargets(limit) {
+      return this._api.getRecentTargets({
+          limit: limit
+      });
+  }
+  
+  /**
+   * 
+   * @param {number} id
+   * @param {string} type
+   */
+  async getTarget(id, type) {
+      const resp = await this._api.getTargetWithHttpInfo(id, type);
+      return resp.response.body;
+  }
+  
+  /**
+   * 
+   * @param {number} id
+   * @param {string} type
+   */
+  async deletePersonalFavorite(id, type) {
+      return this._api.deletePersonalFavorite(id, type);
+  }
+  
+  /**
+   * 
+   * @param {number} limit
+   */
+  async getPersonalFavorites(limit) {
+      return this._api.getPersonalFavorites({
+          limit: limit
+      });
+  }
+  
+  /**
+   * 
+   * @param {TargetInformation} target
+   * @param {string} category
+   */
+  async savePersonalFavorite(target, category) {
+      return this._api.savePersonalFavorite({
+          data: {
+              target: target,
+              category: category
+          }
+      });
+  }
+  
+  async ackRecentMissedCalls() {
+      return this._api.ackRecentMissedCalls();
   }
 }
 
